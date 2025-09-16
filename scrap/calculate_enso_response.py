a6d837d9e9291e45e08a7eb4bc658c7abf0024d6
Glenn Liu <glenn.y.liu@gmail.com>
2025-09-15 15:17:04 -1000

    Make into loop

--- scrap/calculate_enso_response.py
+++ scrap/calculate_enso_response.py
@@ -51,189 +51,196 @@ datpath         = "/home/niu4/gliu8/projects/scrap/TP_crop/"
 expnames        = ["TCo319_ctl1950d","TCo319_ssp585","TCo1279-DART-1950","TCo1279-DART-2090"]
 expnames_long   = ["31km Control","31km SSP585","9km 1950","9km 2090"]
 
-vname           = "tx_sur"#"Dmaxgrad" #"sst"#"str"
-"""
-Some issues to fix with vname
+vname           = "sst"#"Dmaxgrad" #"sst"#"str"
 
-tx_surf_1m  (file name) corresponds to tx_sur
-D20         (file name) corresponds to nz1
-Dmax        (file name) corresponds to nz1
+vnames = ['sst','ssr','str','Dmaxgrad',"D20"]
+nvars = len(vnames)
 
-in one file
-SSR (variable name) --> ssr, and the coordinate names are somewhat messed up
+for vv in range(nvars):
 
-Also, it seems that I spelled separately wrong in the filename, RIP
-
-"""
-
-
-ninopath        = "/home/niu4/gliu8/projects/scrap/nino34/"
-
-nexps           = len(expnames)
-
-#%% Helper Functions
-
-
-def swap_rename(ds,chkvar,newvar):
-    if chkvar in list(ds.coords):
-        print("Renaming [%s] to [%s]" % (chkvar,newvar))
-        ds = ds.rename({chkvar:newvar})
-    return ds
-
-def standardize_names(ds):
+    vname = vnames[vv]
+    """
+    Some issues to fix with vname
     
-    ds = swap_rename(ds,'time_counter','time')
-    ds = swap_rename(ds,"TIME_COUNTER",'time')
-    ds = swap_rename(ds,"LON","lon")
-    ds = swap_rename(ds,"LAT","lat")
-    ds = swap_rename (ds,"LAT232_409","lat")
-    return ds
+    tx_surf_1m  (file name) corresponds to tx_sur
+    D20         (file name) corresponds to nz1
+    Dmax        (file name) corresponds to nz1
     
-# def rename_time_dart(ds):
-#     if 'time_counter' in list(ds.coords):
-#         print("Renaming [time_counter] to [time]")
-#         ds = ds.rename({'time_counter':'time'})
-#     if 'TIME_COUNTER' in list(ds.coords):
-#         print("Renaming [TIME_COUNTER] to [time]")
-#         ds = ds.rename({'TIME_COUNTER':'time'})
-#     return ds
-
-def preprocess_enso(ds):
-    # Remove Mean Seasonal Cycle and the Quadratic Trend
-    dsds   = proc.xrdeseason(ds)
+    in one file
+    SSR (variable name) --> ssr, and the coordinate names are somewhat messed up
     
-    # Compute Spectra
-    def detrend_quadratic(ds):
-        x = np.arange(len(ds))
-        y = ds.data
-        ydetrended,model=proc.detrend_poly(x,y,2)
-        return ydetrended
-        
-    st = time.time()
-    dsanom = xr.apply_ufunc(
-        detrend_quadratic,  # Pass the function
-        dsds,  # The inputs in order that is expected
-        # Which dimensions to operate over for each argument...
-        input_core_dims=[['time'],],
-        output_core_dims=[['time'],],  # Output Dimension
-        vectorize=True,  # True to loop over non-core dims
-        )
-    print("Detrended in %.2fs" % (time.time()-st))
-    #dsanom = proc.xrdetrend_1d(ds,2) 
-    return dsanom
-
-
-def match_time_month(var_in,ts_in):
-    # Crops the start and end times for var_in and ts_in (xr.DataArrays/Datasets)
-    # Note works for datetime64[ns] format in xr.DataArray
+    Also, it seems that I spelled separately wrong in the filename, RIP
     
-    if len(var_in.time) != len(ts_in.time): # Check if they match
+    """
+    
+    
+    ninopath        = "/home/niu4/gliu8/projects/scrap/nino34/"
+    
+    nexps           = len(expnames)
+    
+    #%% Helper Functions
+    
+    
+    def swap_rename(ds,chkvar,newvar):
+        if chkvar in list(ds.coords):
+            print("Renaming [%s] to [%s]" % (chkvar,newvar))
+            ds = ds.rename({chkvar:newvar})
+        return ds
+    
+    def standardize_names(ds):
         
-        # Warning: Only checking Year and Date
-        vstart = str(np.array((var_in.time.data[0])))[:7]
-        tstart = str(np.array((ts_in.time.data[0])))[:7]
+        ds = swap_rename(ds,'time_counter','time')
+        ds = swap_rename(ds,"TIME_COUNTER",'time')
+        ds = swap_rename(ds,"LON","lon")
+        ds = swap_rename(ds,"LAT","lat")
+        ds = swap_rename (ds,"LAT232_409","lat")
+        return ds
         
-        if vstart != tstart:
-            print("Start time (v1=%s,v2=%s) does not match..." % (vstart,tstart))
-            if vstart > tstart:
-                print("Cropping to start from %s" % vstart)
-                ts_in = ts_in.sel(time=slice(vstart+"-01",None))
-            elif vstart < tstart:
-                print("Cropping to start from %s" % tstart)
-                var_in = var_in.sel(time=slice(tstart+"-01",None))
+    # def rename_time_dart(ds):
+    #     if 'time_counter' in list(ds.coords):
+    #         print("Renaming [time_counter] to [time]")
+    #         ds = ds.rename({'time_counter':'time'})
+    #     if 'TIME_COUNTER' in list(ds.coords):
+    #         print("Renaming [TIME_COUNTER] to [time]")
+    #         ds = ds.rename({'TIME_COUNTER':'time'})
+    #     return ds
+    
+    def preprocess_enso(ds):
+        # Remove Mean Seasonal Cycle and the Quadratic Trend
+        dsds   = proc.xrdeseason(ds)
         
-        vend = str(np.array((var_in.time.data[-1])))[:7]
-        tend = str(np.array((ts_in.time.data[-1])))[:7]
-        
-        
-        if vend != tend:
+        # Compute Spectra
+        def detrend_quadratic(ds):
+            x = np.arange(len(ds))
+            y = ds.data
+            ydetrended,model=proc.detrend_poly(x,y,2)
+            return ydetrended
             
-            print("End times (v1=%s,v2=%s) does not match..." % (vend,tend))
+        st = time.time()
+        dsanom = xr.apply_ufunc(
+            detrend_quadratic,  # Pass the function
+            dsds,  # The inputs in order that is expected
+            # Which dimensions to operate over for each argument...
+            input_core_dims=[['time'],],
+            output_core_dims=[['time'],],  # Output Dimension
+            vectorize=True,  # True to loop over non-core dims
+            )
+        print("Detrended in %.2fs" % (time.time()-st))
+        #dsanom = proc.xrdetrend_1d(ds,2) 
+        return dsanom
+    
+    
+    def match_time_month(var_in,ts_in):
+        # Crops the start and end times for var_in and ts_in (xr.DataArrays/Datasets)
+        # Note works for datetime64[ns] format in xr.DataArray
+        
+        if len(var_in.time) != len(ts_in.time): # Check if they match
             
-            if vend > tend:
-                print("\nCropping to start from %s" % tend)
-                var_in = var_in.sel(time=slice())
-            elif vend < tend:
-                print("\nCropping to start from %s" % vend)
-                ts_in = ts_in.sel(time=slice(None,vend+"-31"))
+            # Warning: Only checking Year and Date
+            vstart = str(np.array((var_in.time.data[0])))[:7]
+            tstart = str(np.array((ts_in.time.data[0])))[:7]
+            
+            if vstart != tstart:
+                print("Start time (v1=%s,v2=%s) does not match..." % (vstart,tstart))
+                if vstart > tstart:
+                    print("Cropping to start from %s" % vstart)
+                    ts_in = ts_in.sel(time=slice(vstart+"-01",None))
+                elif vstart < tstart:
+                    print("Cropping to start from %s" % tstart)
+                    var_in = var_in.sel(time=slice(tstart+"-01",None))
+            
+            vend = str(np.array((var_in.time.data[-1])))[:7]
+            tend = str(np.array((ts_in.time.data[-1])))[:7]
+            
+            
+            if vend != tend:
                 
-        print(len(var_in.time) == len(ts_in.time))  
-    return var_in,ts_in
+                print("End times (v1=%s,v2=%s) does not match..." % (vend,tend))
+                
+                if vend > tend:
+                    print("\nCropping to start from %s" % tend)
+                    var_in = var_in.sel(time=slice())
+                elif vend < tend:
+                    print("\nCropping to start from %s" % vend)
+                    ts_in = ts_in.sel(time=slice(None,vend+"-31"))
+                    
+            print(len(var_in.time) == len(ts_in.time))  
+        return var_in,ts_in
+            
+    
+    #%% Part (1) Load the ENSO Indices
+    
+    ds_enso = []
+    for ex in range(nexps):
         
-
-#%% Part (1) Load the ENSO Indices
-
-ds_enso = []
-for ex in range(nexps):
+        ninonc      = "%s%s_nino34.nc" % (ninopath,expnames[ex])
+        ds          = xr.open_dataset(ninonc).load()
+        
+        
+        ds_enso.append(ds)
     
-    ninonc      = "%s%s_nino34.nc" % (ninopath,expnames[ex])
-    ds          = xr.open_dataset(ninonc).load()
+    #%% Part (2) Load the variable of choice
     
+    ds_var = []
+    for ex in tqdm.tqdm(range(nexps)):
+        
+        
+        if vname == "tx_sur":
+            vname_file = "tx_surf_1m"
+            ncname = "%s%s_%s.nc" % (datpath,expnames[ex],vname_file)
+        else:
+            ncname = "%s%s_%s.nc" % (datpath,expnames[ex],vname)
+        ds = xr.open_dataset(ncname)
+        
+        if vname.upper() in list(ds.keys()):
+            print("Renaming %s to %s" % (vname.upper(),vname))
+            ds = ds.rename({vname.upper():vname})
+        
+        ds_var.append(ds)
     
-    ds_enso.append(ds)
-
-#%% Part (2) Load the variable of choice
-
-ds_var = []
-for ex in tqdm.tqdm(range(nexps)):
+    ds_var = [standardize_names(ds) for ds in ds_var]
     
+    #%% Part (3) Deseason and Detrend
     
-    if vname == "tx_sur":
-        vname_file = "tx_surf_1m"
-        ncname = "%s%s_%s.nc" % (datpath,expnames[ex],vname_file)
+    # Takes
+    # 1.32 sec
+    # 13.06 sec (exp2)
+    # 110.36s
+    # 94.95s
+    if vname in ["D20","Dmaxgrad"]:
+        ds_anoms = [preprocess_enso(ds['nz1']) for ds in ds_var]
     else:
-        ncname = "%s%s_%s.nc" % (datpath,expnames[ex],vname)
-    ds = xr.open_dataset(ncname)
+        ds_anoms = [preprocess_enso(ds[vname]) for ds in ds_var]
     
-    if vname.upper() in list(ds.keys()):
-        print("Renaming %s to %s" % (vname.upper(),vname))
-        ds = ds.rename({vname.upper():vname})
     
-    ds_var.append(ds)
-
-ds_var = [standardize_names(ds) for ds in ds_var]
-
-#%% Part (3) Deseason and Detrend
-
-# Takes
-# 1.32 sec
-# 13.06 sec (exp2)
-# 110.36s
-# 94.95s
-if vname in ["D20","Dmaxgrad"]:
-    ds_anoms = [preprocess_enso(ds['nz1']) for ds in ds_var]
-else:
-    ds_anoms = [preprocess_enso(ds[vname]) for ds in ds_var]
-
-
-
-#%% Part (4) Compute the regression pattern, for all months and separately. then save file.
-
-# All Months
-for ex in range(4):
-    ts_in   = ds_enso[ex].sst
-    var_in  = ds_anoms[ex]
     
-    var_in,ts_in = match_time_month(var_in,ts_in)
+    #%% Part (4) Compute the regression pattern, for all months and separately. then save file.
     
-    dsout   = proc.detrend_by_regression(var_in, ts_in, regress_monthly=False,return_pattern_only=True)
-    
-    edict   = proc.make_encoding_dict(dsout)
-    ncout   = "%s%s_%s_ENSO_regression_allmonths.nc" % (datpath,expnames[ex],vname)
-    
-    dsout.to_netcdf(ncout,encoding=edict)
-    
-# Monthly
-for ex in range(4):
-    ts_in   = ds_enso[ex].sst
-    var_in  = ds_anoms[ex]
-    
-    var_in,ts_in = match_time_month(var_in,ts_in)
-    
-    dsout   = proc.detrend_by_regression(var_in, ts_in, regress_monthly=True,return_pattern_only=True)
-    
-    edict   = proc.make_encoding_dict(dsout)
-    ncout   = "%s%s_%s_ENSO_regression_seperatemonths.nc" % (datpath,expnames[ex],vname)
-    
-    dsout.to_netcdf(ncout,encoding=edict)
+    # All Months
+    for ex in range(4):
+        ts_in   = ds_enso[ex].sst
+        var_in  = ds_anoms[ex]
+        
+        var_in,ts_in = match_time_month(var_in,ts_in)
+        
+        dsout   = proc.detrend_by_regression(var_in, ts_in, regress_monthly=False,return_pattern_only=True)
+        
+        edict   = proc.make_encoding_dict(dsout)
+        ncout   = "%s%s_%s_ENSO_regression_allmonths.nc" % (datpath,expnames[ex],vname)
+        
+        dsout.to_netcdf(ncout,encoding=edict)
+        
+    # Monthly
+    for ex in range(4):
+        ts_in   = ds_enso[ex].sst
+        var_in  = ds_anoms[ex]
+        
+        var_in,ts_in = match_time_month(var_in,ts_in)
+        
+        dsout   = proc.detrend_by_regression(var_in, ts_in, regress_monthly=True,return_pattern_only=True)
+        
+        edict   = proc.make_encoding_dict(dsout)
+        ncout   = "%s%s_%s_ENSO_regression_seperatemonths.nc" % (datpath,expnames[ex],vname)
+        
+        dsout.to_netcdf(ncout,encoding=edict)
 
