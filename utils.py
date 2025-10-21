83779455967e44c1a6eb8c4d0ffb8eb398e8d2fe
Glenn Liu <glenn.y.liu@gmail.com>
2025-10-20 14:56:23 -1000

    Add function to extract raw path from awi

--- utils.py
+++ utils.py
@@ -25,6 +25,7 @@ Function                Description
 awi_mean_loader         : (l) load mean/monvar/scycle calculations from calc_mean_patterns_TP 
 calc_lag_regression_1d  : (g) Compute lead lag regression for 1d timeseries
 combine_events          : (g) Given identified events, combine similar events and get other traits (duration, etc)
+get_rawpath_awi         : (A) Get rawpath for AWI output on niu
 init_tp_map             : (v) initialize tropical Pacific plot 
 load_ensoid             : (l) load enso indices calculated by calc_nino34.py
 mcsample                : (g) Monte Carlo Sampler to repeat function
@@ -49,6 +50,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import sys
 import time
+import glob
 
 #%%
 
@@ -190,7 +192,130 @@ def combine_events(var_in,id_in,tol=1,verbose=True):
                    durations=durations,
                    eventmonths=months)
     return outdict
+
+def get_rawpath_awi(expname,vname,ensnum=None):
+    # TCo319_ctl1950d
+    ctlpath0_31= "/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_ctl1950d/monthly/"
+    ctlpath1_31= "//export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_control/"
+
+    # TCo319_ssp585
+    ssppath0_31="/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_ssp585/"
+    ssppath0_31_tropics="/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_ssp585/tropics_only/"
+    if ensnum is not None:
+        ssppath0_31_ens = "/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_ssp585_ens%02i/"
     
+    # TCo1279_DART-1950
+    ctlpath0_09_atm = "/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-1950_9km/atm/mon/"
+    ctlpath0_09_ocn = "/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-1950_9km/ocn/mon/"
+
+    # TCo1279_DART-2090
+    ssppath0_09     = "/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/"
+
+    # TCo2559-DART-1950C
+    ctlpath0_05_atm = "/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo2559-DART-1950C_5km/atm/"
+    ctlpath0_05_ocn = "/export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo2559-DART-1950C_5km/ocn/"
+
+
+    
+    notfound=False
+    # ========================================================
+    if expname == "TCo319_ctl1950d": # 31 km
+        # First Try the Control Path (post 1950 crops)
+        searchstr = "%s/*_%s_*.nc" % (ctlpath0_31,vname)
+        nclist    = glob.glob(searchstr)
+        if len(nclist) > 0:
+            print("\tFound in [TCo319_ctl1950d] path!")
+            #continue
+        else:
+            print("\tNot found in [TCo319_ctl1950d] path...")
+            # Next Try the older Control Path
+            searchstr = "%s/*_%s_*.nc" % (ctlpath1_31,vname)
+            nclist    = glob.glob(searchstr)
+            if len(nclist) > 0:
+                print("\tFound in [TCo319_control] path!")
+            else:
+                notfound=True
+    # ========================================================
+    elif expname == "TCo319_ssp585":
+        if ensnum is not None:
+            searchstr = ssppath0_31_ens % (ensnum) + "*_%s_*.nc" % (vname)
+            nclist    = glob.glob(searchstr)
+            print("\tFound in [TCo319_ssp585_ens%02i]!" % ensnum)
+            #continue
+        else:
+            print("\tVariable not available for ens members... looking in original folder")
+        
+        # First Try the Control Path (post 1950 crops)
+        searchstr = "%s/*_%s_*.nc" % (ssppath0_31,vname)
+        nclist    = glob.glob(searchstr)
+        if len(nclist) > 0:
+            print("\tFound in [TCo319_ssp585] path!")
+            #continue
+        else: # Otherwise Try Tropics Only
+            print("\tNot found in [TCo319_ssp585] path...")
+            # First Try the Control Path (post 1950 crops)
+            searchstr = "%s/*_%s_*.nc" % (ssppath0_31_tropics,vname)
+            nclist    = glob.glob(searchstr)
+            if len(nclist) > 0:
+                print("\tFound in [TCo319_ssp585_tropics_only] path!")
+                #continue
+            else:
+                notfound=True
+    # ========================================================
+    elif expname == "TCo1279-DART-1950":
+        # First Try the ATM Path (post 1950 crops)
+        searchstr = "%s/*_%s_*.nc" % (ctlpath0_09_atm,vname)
+        nclist    = glob.glob(searchstr)
+        if len(nclist) > 0:
+            print("\tFound in [TCo1279-DART-1950_atm] path!")
+            #continue
+        else:
+            print("\tNot found in [TCo1279-DART-1950_atm] path...")
+            # First Try the ATM Path (post 1950 crops)
+            searchstr = "%s/*_%s_*.nc" % (ctlpath0_09_ocn,vname)
+            nclist    = glob.glob(searchstr)
+            if len(nclist) > 0:
+                print("\tFound in [TCo1279-DART-1950_ocn] path!")
+                #continue
+            else:
+                notfound=True
+    # ========================================================
+    elif expname == "TCo1279-DART-2090":
+        # First Try the Control Path (post 1950 crops)
+        searchstr = "%s/*_%s_*.nc" % (ssppath0_09,vname)
+        nclist    = glob.glob(searchstr)
+        if len(nclist) > 0:
+            print("\tFound in [TCo319_ctl1950d] path!")
+        else:
+            notfound=True
+    # ========================================================
+    elif expname == "TCo2559-DART-1950C":
+        # First Try the ATM Path (post 1950 crops)
+        searchstr = "%s/*_%s_*.nc" % (ctlpath0_05_atm,vname)
+        nclist    = glob.glob(searchstr)
+        if len(nclist) > 0:
+            print("\tFound in [TCo2559-DART-1950C_atm] path!")
+            #continue
+        else:
+            print("\tNot found in [TCo2559-DART-1950C_atm] path...")
+            # First Try the ATM Path (post 1950 crops)
+            searchstr = "%s/*_%s_*.nc" % (ctlpath0_05_ocn,vname)
+            nclist    = glob.glob(searchstr)
+            if len(nclist) > 0:
+                print("\tFound in [TCo2559-DART-1950C_ocn] path!")
+                #continue
+            else:
+                notfound=True
+    # ========================================================
+    else:
+        print("%s not recognized..." % expname)
+        notfound=True
+    if notfound:
+        print("%s Not found for %s" % (vname,expname))
+        return None
+    print(nclist)
+    return None    
+
 def load_ensoid(expname,ninoid_name='nino34',datpath=None,standardize=True):
     # Load Enso indices calculated with calc_nino34.py
     if datpath is None:
