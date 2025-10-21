55f0cadc51956deeac2756345d8684d9bc305c8b
Glenn Liu <glenn.y.liu@gmail.com>
2025-10-20 18:11:19 -1000

    Rerun for global calculation

--- scrap/calc_EIS_AWI.py
+++ scrap/calc_EIS_AWI.py
@@ -43,6 +43,9 @@ import utils as ut
 
 #%% Datpaths
 
+TP_crop         = False # Set True to calculate using Tropical Pacific Values
+
+
 expnames        = ["TCo319_ctl1950d","TCo319_ssp585","TCo1279-DART-1950","TCo1279-DART-2090","TCo2559-DART-1950C"]
 expnames_long   = ["31km Control","31km SSP585","9km 1950","9km 2090","5km 1950"]
 
@@ -53,6 +56,7 @@ ex              = 4
 
 # Only available for the 5km simulation
 datpath         = "/home/niu4/gliu8/projects/scrap/TP_crop/"
+datpath_glob    = "/home/niu4/gliu8/projects/scrap/processed_global/"
 vnames          = ["skt","pl_t_700"]
 expname         = "TCo2559-DART-1950C"
 
@@ -61,11 +65,19 @@ expname         = "TCo2559-DART-1950C"
 #%% First, just try for the tropical pacific
 # need to load in T700 and T0 (skt is ok?)
 
+
 dsall = []
-for v in range(2):
-    ncname = "%s%s_%s.nc" % (datpath,expname,vnames[v])
-    ds = xr.open_dataset(ncname).load()
-    dsall.append(ds)
+if TP_crop: # Load just the tropical version
+    for v in range(2):
+        ncname = "%s%s_%s.nc" % (datpath,expname,vnames[v])
+        ds = xr.open_dataset(ncname).load()
+        dsall.append(ds)
+else:
+    for v in range(2):
+        vname  = vnames[v]
+        ncname = ut.get_rawpath_awi(expname,vname,ensnum=None)[0]
+        ds = xr.open_dataset(ncname).load()
+        dsall.append(ds)
 
 skt,t700    = dsall
 skt         = skt.skt
@@ -77,46 +89,52 @@ print(np.any(skt < 0))
 
 #%% Check they are in Kelvin
 
-eis = climlab.thermo.EIS(skt.data,t700.data)
+eis    = climlab.thermo.EIS(skt.data,t700.data)
 
 coords = dict(time=skt.time_counter.data,lat=skt.lat,lon=skt.lon)#skt.coords
 ds_eis = xr.DataArray(eis,coords=coords,dims=coords,name="eis")
 
-edict  = proc.make_encoding_dict(ds_eis)
+edict   = proc.make_encoding_dict(ds_eis)
 
-outname = "%s%s_eis_useskt.nc" % (datpath,expname)
+if TP_crop:
+    outname = "%s%s_eis_useskt.nc" % (datpath,expname)
+else:
+    outname = "%s%s_eis_useskt.nc" % (datpath_glob,expname)
 ds_eis.to_netcdf(outname,encoding=edict)
 
 
-#%% Reload if already calculated
-
-outname = "%s%s_eis_useskt.nc" % (datpath,expname)
-ds_eis = xr.open_dataset(outname).eis.load()
 
 
 
+# #%% Reload if already calculated
 
-#%%
-eismean = ds_eis.mean('time')
+# outname = "%s%s_eis_useskt.nc" % (datpath,expname)
+# ds_eis = xr.open_dataset(outname).eis.load()
 
-#%% Visualize the time-mean eis
 
-proj    = ccrs.PlateCarree()
-cints   = np.arange(-8,8.5,0.5)
-fig,ax  = ut.init_tp_map()
-plotvar = eismean
-pcm     = ax.pcolormesh(plotvar.lon,plotvar.lat,plotvar,transform=proj,vmin=cints[0],vmax=cints[-1],cmap='cmo.balance')
-cl      = ax.contour(plotvar.lon,plotvar.lat,plotvar,transform=proj,levels=cints,
-                     colors="k",linewidths=0.75)
 
-cb      = viz.hcbar(pcm,ax=ax,fraction=0.045,pad=0.1)
-cb.set_label("Mean Estimated Inversion Strength (%s)" % expnames[ex])
-ax.clabel(cl,levels=cints[::2],fontsize=8)
 
-plt.show()
+# #%%
+# eismean = ds_eis.mean('time')
 
-#%% Load in ENSO Indices
+# #%% Visualize the time-mean eis
 
-ensoid   = ut.load_ensoid(expname,"nino34",standardize=False)
+# proj    = ccrs.PlateCarree()
+# cints   = np.arange(-8,8.5,0.5)
+# fig,ax  = ut.init_tp_map()
+# plotvar = eismean
+# pcm     = ax.pcolormesh(plotvar.lon,plotvar.lat,plotvar,transform=proj,vmin=cints[0],vmax=cints[-1],cmap='cmo.balance')
+# cl      = ax.contour(plotvar.lon,plotvar.lat,plotvar,transform=proj,levels=cints,
+#                      colors="k",linewidths=0.75)
+
+# cb      = viz.hcbar(pcm,ax=ax,fraction=0.045,pad=0.1)
+# cb.set_label("Mean Estimated Inversion Strength (%s)" % expnames[ex])
+# ax.clabel(cl,levels=cints[::2],fontsize=8)
+
+# plt.show()
+
+# #%% Load in ENSO Indices
+
+# ensoid   = ut.load_ensoid(expname,"nino34",standardize=False)
 
 
