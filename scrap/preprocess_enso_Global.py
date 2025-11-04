26a0d1b55762e8238608083aad7b3b7bb036ca96
Glenn Liu <glenn.y.liu@gmail.com>
2025-11-03 15:59:23 -1000

    Add loop by experiment

--- scrap/preprocess_enso_Global.py
+++ scrap/preprocess_enso_Global.py
@@ -41,17 +41,17 @@ import utils as ut
 #%% 
 
 # # Simulation Names -----
-# expnames        = ["TCo319_ctl1950d","TCo319_ssp585","TCo1279-DART-1950","TCo1279-DART-2090","TCo2559-DART-1950C"]
+expnames        = ["TCo319_ctl1950d","TCo319_ssp585","TCo1279-DART-1950","TCo1279-DART-2090","TCo2559-DART-1950C"]
 # expnames_long   = ["31km Control","31km SSP585","9km 1950","9km 2090","5km 1950"]
-# timecrops       = [None,]#[[1950,2100],None,None,None,None]
+timecrops       = [[1950,2100],None,None,None,None]
 # vnames          = ['sst',]#["ttcre","tscre"]#['sst','str','ssr','skt','ssh','lcc','tcc','ttr','ttrc','tsr','tsrc']
-# nexps           = len(expnames)
+nexps           = len(expnames)
 
 #timecrops       = #[]
 
 #%% Set data and outpath
 
-regrid_1x1      = False
+regrid_1x1      = True
 
 if regrid_1x1:
     print("Using Regridded Output!")
@@ -66,103 +66,108 @@ else:
 
 #%% Load Variable
 
-expname         = "TCo319_ssp585" #"TCo2559-DART-1950C" #"TCo319_ctl1950d" #"TCo2559-DART-1950C" #"TCo1279-DART-1950" #"TCo319_ctl1950d" #"TCo1279-DART-1950" #"TCo2559-DART-1950C" #"TCo319_ssp585"
-timecrop        = None#[1950,2100] # None
-vnames          = ['ttr','ttrc','tsr','tsrc',]#'sst']
-#vname          = "sst"
-for vname in vnames:
+#expname         = "TCo319_ctl1950d" #"TCo2559-DART-1950C" #"TCo319_ctl1950d" #"TCo2559-DART-1950C" #"TCo1279-DART-1950" #"TCo319_ctl1950d" #"TCo1279-DART-1950" #"TCo2559-DART-1950C" #"TCo319_ssp585"
+#timecrop        = None#[1950,2100] # None
+
+for ex,expname in enumerate(expnames):
     
-    if not regrid_1x1:
-        nclist          = ut.get_rawpath_awi(expname,vname,ensnum=None)
-        print("Found the following:")
-        print("\tTaking the first found file: %s" % nclist[0])
-        ncname          = nclist[0]
-        
-        #% Load Seasonal Cycle for Deseasonalizing (calculated from calc_mean_patterns_General.py)
-        # ncsearch        = "%s%s_%s*.nc" % (summarypath,expname,vname)
-        # ncsummary       = glob.glob(ncsearch)
-        # print(ncsummary)
-        # print("Found the following:")
-        # ncsummary       = ncsummary[0]
-        # print("\tTaking the first found file: %s" % ncsummary)
-        # ds_scycle       = xr.open_dataset(ncsummary).scycle # Get seasonal scycle
-        
-        #%% Open File and Chunk
-        dsvar           = xr.open_dataset(ncname)[vname]#[vname]#[vname]
-        
-    else:
-        
-        ncname = "%s%s_%s_regrid1x1.nc" % (datpath,expname,vname)# TCo1279-DART-1950_tsrc_regrid1x1.nc
-        dsvar  = xr.open_dataset(ncname)[vname]
-        
+    timecrop = timecrops[ex]
     
-    # Crop time (mostly for control run, pre 1950)
-    if timecrop is not None:
-        try:
-            print("Cropping time for %s: %s to %s" % (expname,str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
-            dsvar = dsvar.sel(time_counter=slice(str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
-            timename    = "time_counter"
-        except:
-            print("Renaming variables first")
-            # Standardize the names
-            dsvar = ut.standardize_names(dsvar)
-            print("Cropping time for %s: %s to %s" % (expname,str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
-            dsvar = dsvar.sel(time=slice(str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
-            timename    = "time"
-    else:
-        if "time_counter" in list(dsvar.coords):
-            timename = "time_counter"
+    vnames          = ['eis','allsky','cre','clearsky',]#['ttr','ttrc','tsr','tsrc',]#'sst']
+    #vname          = "sst"
+    for vname in tqdm.tqdm(vnames):
+        
+        if not regrid_1x1:
+            nclist          = ut.get_rawpath_awi(expname,vname,ensnum=None)
+            print("Found the following:")
+            print("\tTaking the first found file: %s" % nclist[0])
+            ncname          = nclist[0]
+            
+            #% Load Seasonal Cycle for Deseasonalizing (calculated from calc_mean_patterns_General.py)
+            # ncsearch        = "%s%s_%s*.nc" % (summarypath,expname,vname)
+            # ncsummary       = glob.glob(ncsearch)
+            # print(ncsummary)
+            # print("Found the following:")
+            # ncsummary       = ncsummary[0]
+            # print("\tTaking the first found file: %s" % ncsummary)
+            # ds_scycle       = xr.open_dataset(ncsummary).scycle # Get seasonal scycle
+            
+            #%% Open File and Chunk
+            dsvar           = xr.open_dataset(ncname)[vname]#[vname]#[vname]
+            
         else:
-            dsvar = ut.standardize_names(dsvar)
-            timename = "time"
-    
-    # Set up Chunking
-    dsvar           = dsvar.chunk(dict(lat='auto',lon='auto'))
-    
-    #%% Set up function and preprocess
-    def preprocess_enso_point(ts):
-        ntime   = len(ts)
-        nyr     = int(ntime/12)
-        x       = np.arange(ntime)
+            
+            ncname = "%s%s_%s_regrid1x1.nc" % (datpath,expname,vname)# TCo1279-DART-1950_tsrc_regrid1x1.nc
+            dsvar  = xr.open_dataset(ncname)[vname]
+            
         
-        tsrs    = ts.reshape((nyr,12))
-        tsrsa   = tsrs - tsrs.mean(0)[None,:]
+        # Crop time (mostly for control run, pre 1950)
+        if timecrop is not None:
+            try:
+                print("Cropping time for %s: %s to %s" % (expname,str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
+                dsvar = dsvar.sel(time_counter=slice(str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
+                timename    = "time_counter"
+            except:
+                print("Renaming variables first")
+                # Standardize the names
+                dsvar = ut.standardize_names(dsvar)
+                print("Cropping time for %s: %s to %s" % (expname,str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
+                dsvar = dsvar.sel(time=slice(str(timecrop[0])+'-01-01',str(timecrop[1])+'-12-31'))
+                timename    = "time"
+        else:
+            if "time_counter" in list(dsvar.coords):
+                timename = "time_counter"
+            else:
+                dsvar = ut.standardize_names(dsvar)
+                timename = "time"
         
-        tsa     = tsrsa.flatten()
+        # Set up Chunking
+        dsvar           = dsvar.chunk(dict(lat='auto',lon='auto'))
         
-        #tsa     = proc.deseason(ts)
-        if np.any(np.isnan(ts)):
-            return np.ones(tsa.shape)*np.nan
-        ydetrended,model=proc.detrend_poly(x,tsa,2)
+        #%% Set up function and preprocess
+        def preprocess_enso_point(ts):
+            ntime   = len(ts)
+            nyr     = int(ntime/12)
+            x       = np.arange(ntime)
+            
+            tsrs    = ts.reshape((nyr,12))
+            tsrsa   = tsrs - tsrs.mean(0)[None,:]
+            
+            tsa     = tsrsa.flatten()
+            
+            #tsa     = proc.deseason(ts)
+            if np.any(np.isnan(ts)):
+                return np.ones(tsa.shape)*np.nan
+            ydetrended,model=proc.detrend_poly(x,tsa,2)
+            
+            return ydetrended
         
-        return ydetrended
-    
-    # Set Up Function
-    dsanom = xr.apply_ufunc(
-        preprocess_enso_point,  # Pass the function
-        dsvar,  # The inputs in order that is expected
-        # Which dimensions to operate over for each argument...
-        input_core_dims=[[timename],],
-        output_core_dims=[[timename],],  # Output Dimension
-        vectorize=True,  # True to loop over non-core dims
-        dask='parallelized',
-        output_dtypes='float32',
-        )
-    
-    # 520.45s, 492.45, 5875.68s
-    st = time.time()
-    dsanom = dsanom.compute()
-    print("Preprocessed in %.2fs" % (time.time()-st))
-    
-    #%% Save Output
-    st      = time.time()
-    outname = "%s%s_%s_anom.nc" % (outpath,expname,vname)
-    edict   = proc.make_encoding_dict(dsanom)
-    dsanom.to_netcdf(outname,encoding=edict)
-    proc.printtime(st,print_str="\tsaved")
-    
-    dsanom.close()
-    dsvar.close()
-    
+        # Set Up Function
+        dsanom = xr.apply_ufunc(
+            preprocess_enso_point,  # Pass the function
+            dsvar,  # The inputs in order that is expected
+            # Which dimensions to operate over for each argument...
+            input_core_dims=[[timename],],
+            output_core_dims=[[timename],],  # Output Dimension
+            vectorize=True,  # True to loop over non-core dims
+            dask='parallelized',
+            output_dtypes='float32',
+            )
+        
+        # 520.45s, 492.45, 5875.68s
+        st = time.time()
+        dsanom = dsanom.compute()
+        print("Preprocessed in %.2fs" % (time.time()-st))
+        
+        #%% Save Output
+        st      = time.time()
+        outname = "%s%s_%s_anom.nc" % (outpath,expname,vname)
+        edict   = proc.make_encoding_dict(dsanom)
+        dsanom.to_netcdf(outname,encoding=edict)
+        proc.printtime(st,print_str="\tsaved")
+        
+        dsanom.close()
+        dsvar.close()
+        
 
 
