29be5b4686289b9c7264394cb904a81c046bb87d
Glenn Liu <glenn.y.liu@gmail.com>
2025-11-17 11:07:56 -1000

    Update to match bash syntax

--- calculations/anomalize_detrend_era5.sh
+++ calculations/anomalize_detrend_era5.sh
@@ -7,14 +7,14 @@ rawpath='/home/niu4/gliu8/share/ERA5/processed'
 scyclepath='/home/niu4/gliu8/projects/common_data/ERA5/scycle'
 detrendpath='/home/niu4/gliu8/projects/common_data/ERA5/anom_detrend1'
 
-for vname in $vnames; do
+for vname in ${vnames[@]}; do
   
     infile=$(${rawpath}/${vname}_${yearstr}.nc)
     scyclefile=$(${scyclepath}/${vname}_${yearstr}.nc)
     outfile=$(${detrendpath}/${vname}_${yearstr}.nc)
 
-    cdo ymonmean $infile $scyclefile
-    cdo ymonsub $infile $scyclefile ${detrendpath}/temp1.nc
-    cdo detrend ${detrendpath}/temp1.nc $outfile
+    cdo ymonmean ${infile} ${scyclefile}
+    cdo ymonsub ${infile} ${scyclefile} ${detrendpath}/temp1.nc
+    cdo detrend ${detrendpath}/temp1.nc ${outfile}
 
 done
