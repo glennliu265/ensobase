b0db598e5e47a2d9796806a63595e4cca5c2c1a0
Glenn Liu <glenn.y.liu@gmail.com>
2025-11-17 23:30:28 -1000

    Take time mean of all CCFs

---
+++ calculations/ccf_summary_era5.sh
@@ -0,0 +1,13 @@
+
+vnames=("sst" "eis" "ws10" "Tadv" "w700" "r700") 
+rawpath='/home/niu4/gliu8/share/ERA5/processed'
+outpath='/home/niu4/gliu8/projects/ccfs/time_mean/'
+
+for vname in ${vnames[@]}; do
+  
+    infile=${rawpath}/${vname}_${yearstr}.nc
+    outfile=${outpath}/${vname}_${yearstr}.nc
+    
+    cdo selyear,1979/2024 ${infile} ${outpath}/temp.nc
+    cdo timmean ${outpath}/temp.nc ${outfile}
+done
