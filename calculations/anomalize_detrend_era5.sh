5b41d7916b782a109a2a3874466ddb84126a3287
Glenn Liu <glenn.y.liu@gmail.com>
2025-11-17 15:00:23 -1000

    preprocess sst

--- calculations/anomalize_detrend_era5.sh
+++ calculations/anomalize_detrend_era5.sh
@@ -2,7 +2,7 @@
 
 # Set the variables
 #vnames=("skt" "tsr" "ttr" "tsrc" "ttrc" "u10" "v10" "t700" "w700" "r700")
-vnames=("allsky" "clearsky" "cre" "ttcre" "tscre")
+vnames=("sst") #("allsky" "clearsky" "cre" "ttcre" "tscre")
 yearstr="1979_2024"
 rawpath='/home/niu4/gliu8/share/ERA5/processed'
 scyclepath='/home/niu4/gliu8/projects/common_data/ERA5/scycle'
