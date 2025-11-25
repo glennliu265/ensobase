93d612b3ed83f5cbd69808163b67a935a73f25d4
Glenn Liu <glenn.y.liu@gmail.com>
2025-11-24 16:47:02 -1000

    Regrid ERA5 unanomalized TOA fluxes
    For comparison with CERES EBAF

--- calculations/regrid_era5_1deg.sh
+++ calculations/regrid_era5_1deg.sh
@@ -1,7 +1,13 @@
 
-vnames=("creln") #"sst" "eis" "ws10" "Tadv" "w700" "r700" "cre" "mask") 
-rawpath='/home/niu4/gliu8/projects/common_data/ERA5/anom_detrend1'
-outpath='/home/niu4/gliu8/projects/common_data/ERA5/regrid_1x1/anom_detrend1'
+# # Regrid for anomalized and detrended
+# vnames=("creln") #"sst" "eis" "ws10" "Tadv" "w700" "r700" "cre" "mask") 
+# rawpath='/home/niu4/gliu8/projects/common_data/ERA5/anom_detrend1'
+# outpath='/home/niu4/gliu8/projects/common_data/ERA5/regrid_1x1/anom_detrend1'
+
+# Regrid for raw data
+vnames=("allsky" "clearsky" "cre" "ttcre" "tscre" "ttr" "ttrc" "tsr" "tsrc") #"sst" "eis" "ws10" "Tadv" "w700" "r700" "cre" "mask") 
+rawpath='/home/niu4/gliu8/share/ERA5/processed'
+outpath='/home/niu4/gliu8/projects/common_data/ERA5/regrid_1x1'
 yearstr="1979_2024"
 
 for vname in ${vnames[@]}; do
