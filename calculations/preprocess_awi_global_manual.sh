d86ec5e2b87c2efd1717338ba1efd2a6fe4b00e8
Glenn Liu <glenn.y.liu@gmail.com>
2025-12-04 18:07:30 -1000

    Start processing data for additional kernel calculations

--- calculations/preprocess_awi_global_manual.sh
+++ calculations/preprocess_awi_global_manual.sh
@@ -1,5 +1,7 @@
 
+#  ===================================
 # 9km (1950)
+#  ===================================
 
 # Rename \10ws --> \ws10
 cdo chname,\10ws,ws10 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-1950_9km/TCo1279-DART-1950_atm_remapped_1m_10ws_240months.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-1950_ws10.nc
@@ -13,6 +15,37 @@ cdo chname,\10v,v10 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DAR
 # Copy over SST (9km)
 cp /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-1950_9km/atm/mon/TCo1279-DART-1950_atm_remapped_1m_sst_240months.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-1950_sst.nc
 
-# 9km (2090)
 
+# Copy over Tropospheric Variables
+cdo chname,t,t700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-1950_9km/atm/mon/TCo1279-DART-1950_atm_remapped_1m_t700_240months.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-1950_t700.nc
+cdo chname,r,r700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-1950_9km/atm/mon/TCo1279-DART-1950_atm_remapped_1m_r700_240months.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-1950_r700.nc
+cdo chname,w,w700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-1950_9km/atm/mon/TCo1279-DART-1950_atm_remapped_1m_w700_240months.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-1950_w700.nc
+
+
+
+#  ===================================
+# 9km (2090)
+#  ===================================
+
+# Tropospheric Variables (rename and move to processed folder)
+cdo chname,t,t700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/TCo1279-2090slice_aleph_pl_t_700_1m_2090-2099.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-2090_t700.nc
+cdo chname,r,r700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/TCo1279_2090slice_aleph_pl_r_700_1m_2090-2099.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-2090_r700.nc
+cdo chname,w,w700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/TCo1279-2090slice_aleph_pl_w_700_1m_2090-2099.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-2090_w700.nc
+
+# Wind Variables
+cdo chname,\10ws,ws10 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/TCo1279-2090slice_aleph_10ws_1m_2090-2099.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-2090_ws10.nc
+cdo chname,\10u,u10 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/TCo1279-2090slice_aleph_10u_1m_2090-2099.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-2090_u10.nc
+cdo chname,\10v,v10 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/TCo1279-2090slice_aleph_10v_1m_2090-2099.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-2090_v10.nc
+
+# Copy over sst
+cp /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo1279-DART-2090_9km/TCo1279_2090slice_aleph_sst_1m_2090-2099.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo1279-DART-2090_sst.nc
+
+#  ===================================
+# 31km (Control) 
+#  ===================================
+
+# Tropospheric Variables (rename and move to processed folder)
+cdo chname,t,t700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_ctl1950d/monthly/awicm3_tco319_ctl1950d_pl_t_700_1950-2100.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo319_ctl1950d_t700.nc
+cdo chname,r,r700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_ctl1950d/monthly/awicm3_tco319_ctl1950d_pl_r_700_1950-2100.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo319_ctl1950d_r700.nc
+cdo chname,w,w700 /export/niu2/stuecker/MODELOUTPUT/awicm3_highres/TCo319_ctl1950d/monthly/awicm3_tco319_ctl1950d_pl_w_700_1950-2100.nc /home/niu4/gliu8/projects/scrap/processed_global/TCo319_ctl1950d_w700.nc
 
