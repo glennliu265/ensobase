35efc0d76f1836cbf56f4f89407e67c7d66f666f
Glenn Liu <glenn.y.liu@gmail.com>
2026-08-27 12:04:00 -1000

    Add Windowsize_support

--- mesaclip/calc_doy_xrcorr.py
+++ mesaclip/calc_doy_xrcorr.py
@@ -52,7 +52,9 @@ dsview = dsview.convert_calendar('noleap') # Remove Leap Year
 print("Loaded Data in %.2fs" % (time.time()-st))
 print(dsview)
 
-outdir = "/home/niu4/gliu8/projects/mesaclip/memory/oisst_byday/"
+outdir  = "/home/niu4/gliu8/projects/mesaclip/memory/oisst_byday/"
+
+winsize = 5
 
 #%%
 
@@ -67,46 +69,83 @@ years   = ds.time.dt.year
 ystart  = years[0].data.item()
 yend    = years[-1].data.item()
 
-
-lagcorr_alldoys = []
-for doybase in tqdm(np.arange(1,366)):
+if winsize == 0:
     
-    base_var    = ds.sel(time=ds.time.dt.dayofyear.isin(doybase))
-    lagcorr_day = []
-    
-    for ll in range(nlags):
+    #lagcorr_alldoys = []
+    for doybase in tqdm(np.arange(1,366)):
         
-        # Select Lag Variable
-        doylag = doybase + ll
-        if doylag > 365:
-            doylag = doylag % 365
-            year_cross = True
-        else:
-            year_cross = False
-        lag_var  = ds.sel(time=ds.time.dt.dayofyear.isin(doylag))
+        base_var    = ds.sel(time=ds.time.dt.dayofyear.isin(doybase))
         
-        if year_cross:
-            base_slice  = ["%04i-01-01" % (ystart), "%04i-12-31" % (yend-1)] 
-            lag_slice   = ["%04i-01-01" % (ystart+1), "%04i-12-31" % (yend)] 
+        lagcorr_day = []
+        for ll in range(nlags):
             
-            base_var_in = base_var.sel(time=slice(*base_slice))
-            lag_var_in  = lag_var.sel(time=slice(*lag_slice))
+            # Select Lag Variable
+            doylag = doybase + ll
+            if doylag > 365:
+                doylag = doylag % 365
+                year_cross = True
+            else:
+                year_cross = False
+            lag_var  = ds.sel(time=ds.time.dt.dayofyear.isin(doylag))
+            
+            if year_cross:
+                base_slice  = ["%04i-01-01" % (ystart), "%04i-12-31" % (yend-1)] 
+                lag_slice   = ["%04i-01-01" % (ystart+1), "%04i-12-31" % (yend)] 
+                
+                base_var_in = base_var.sel(time=slice(*base_slice))
+                lag_var_in  = lag_var.sel(time=slice(*lag_slice))
+            
+            else:
+                base_var_in = base_var
+                lag_var_in  = lag_var
+            
+            lag_var_in['time'] = base_var_in['time']
+            
+            corrout = xr.corr(base_var_in,lag_var_in,dim='time')
+            lagcorr_day.append(corrout)
+            
+        lagcorr_day = xr.concat(lagcorr_day,dim='lag')
         
-        else:
-            base_var_in = base_var
-            lag_var_in  = lag_var
-        
-        lag_var_in['time'] = base_var_in['time']
-        
-        corrout = xr.corr(base_var_in,lag_var_in,dim='time')
-        lagcorr_day.append(corrout)
-        
-    lagcorr_day = xr.concat(lagcorr_day,dim='lag')
+        outname = "%sdaily_acf_lagmax%i_nowindow_doy%03i.nc" % (outdir,nlags,doybase)
+        lagcorr_day.to_netcdf(outname)
+else:
+    print("Running script with window size")
     
-    outname = "%sdaily_acf_lagmax%i_nowindow_doy%03i.nc" % (outdir,nlags,doybase)
-    lagcorr_day.to_netcdf(outname)
-    
+    for doybase in tqdm(np.arange(1,366)):
         
+        
+        # Construct Base Window
+        baseinfo    = proc.construct_window_doy(doybase,winsize,verbose=False)
+        
+        lagcorr_day = []
+        for ll in tqdm(range(nlags)):
+            
+            # Select Lag Variable
+            doylag = doybase + ll
+            if doylag > 365:
+                doylag = doylag % 365
+                year_cross = True
+            else:
+                year_cross = False
+            
+            # Construct Lag Widnow
+            laginfo = proc.construct_window_doy(doylag,winsize,verbose=False)
+            
+            # Perform Indexing
+            basevar,lagvar = proc.index_year_base_lag_xr(ds,baseinfo,laginfo,ll,verbose=False)
+            
+            # Calcualte xrcorr
+            lagvar['time'] = basevar['time']
+            corrout = xr.corr(basevar,lagvar,dim='time')
+            lagcorr_day.append(corrout)
+        
+        lagcorr_day = xr.concat(lagcorr_day,dim='lag')
+        outname = "%sdaily_acf_lagmax%i_winsize%02i_doy%03i.nc" % (outdir,nlags,winsize,doybase)
+        lagcorr_day.to_netcdf(outname)
+        
+        # End DOY Loop
+
+print("Completed Script in %.2fs" % (time.time()-st_all))
     
 
 # #%% Quick Test if I just use xrcorr
