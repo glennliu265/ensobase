bcf0f9902705cf14ce4b4c293c21f91b7504fe33
Glenn Liu <glenn.y.liu@gmail.com>
2026-08-28 14:43:58 -1000

    Add hires support
    Load hires data
    Fix Lat/Lon

--- mesaclip/calc_doy_xrcorr_blockwise.py
+++ mesaclip/calc_doy_xrcorr_blockwise.py
@@ -77,7 +77,14 @@ def fix_mesaclip_lr_date(ds):
     ds_dummy         = xr.where(np.isnan(ds.isel(time=1)),np.nan,0)
     #return xr.combine_by_coords([ds,ds_dummy])
     return xr.concat([dsens,ds_dummy],dim='time').sortby('time')
-        
+
+def fix_latlon_hires(ds):
+    renamedict = dict(nlat='lat',nlon='lon')
+    #ds        = standardize_names(ds)
+    ds        = ds.rename(renamedict)
+    ds['lon'] = np.arange(0,360,1)
+    ds['lat'] = np.arange(-90,90,1)
+    return ds
     
 # def convert_size(size_bytes):
 #    # https://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
@@ -98,14 +105,15 @@ doy_monmid    = doy_istart + 14
 doys_sel      = np.sort(np.hstack([doy_istart,doy_monmid]))
 winsize       = 0
 
-ndoys = len(doys_sel)
+ndoys         = len(doys_sel)
 #%% Load View of Dataset
 
 st            = time.time()
 
 datpath       = "/home/niu4/gliu8/share/CESM1/MESACLIP/processed/anom_detrend2_19820101-20251231/"
-expname       = "mesaclip_lores"
+expname       = "mesaclip_hires" #"mesaclip_lores"
 vname         = "SST"
+
 
 xrname        = "__xarray_dataarray_variable__"
 ncsearch      = "%s%s_day_1_%s_anom_ens*.nc" % (datpath,expname,vname,)
@@ -114,27 +122,41 @@ nclist,nens   = proc.get_nclist(ncsearch,verbose=True)
 dsall = xr.open_mfdataset(ncsearch,concat_dim='ens',combine='nested')[xrname]
 print(dsall)
 
+
+if expname == "mesaclip_hires":
+    # Reset the values for hr
+    rst = time.time()
+    dsall = fix_latlon_hires(dsall)
+    print("Reformat hires in %.2fs" % (time.time()-rst))
+    
+    
+
 print("View opened in %.2fs" % (time.time()-st))
 
-outdir_base  = "/home/niu4/gliu8/projects/mesaclip/memory/%s/" % expname
+
+outdir_base   = "/home/niu4/gliu8/projects/mesaclip/memory/%s/" % expname
 proc.makedir(outdir_base)
 
 
 #%% Set Up Blocksize and output Directory
 
-bsz       = 20
+if expname == "mesaclip_hires":
+    
+    bsz       = 60
+else:
+    bsz       = 20
 
 # Set Both to same for simplicity
 latsize   = bsz 
 lonsize   = bsz
-lonblocks = np.arange(0,361,lonsize).astype(int)
-latblocks = np.arange(-90,91,latsize).astype(int)
+lonblocks = np.arange(0,360+lonsize,lonsize).astype(int)
+latblocks = np.arange(-90,90+latsize,latsize).astype(int)
 
 nx        = len(lonblocks)
 ny        = len(latblocks)
 ntotal    = nx*ny
 
-ii        = -5
+ii        = -2
 bbsel     = [lonblocks[ii],lonblocks[ii+1],latblocks[ii],latblocks[ii+1]]
 dsreg     = proc.sel_region_xr(dsall,bbsel)
 
@@ -143,9 +165,8 @@ print("Sample Arr: %s" % (dsreg))
 
 #%% Set Output Files
 
-bsz          = 20
-
-outdir_temp  ="%stemp_blocksize%02i_nblocks%i/" % (outdir_base,bsz,ntotal)
+#bsz          = 20
+outdir_temp  = "%stemp_blocksize%02i_nblocks%i/" % (outdir_base,bsz,ntotal)
 proc.makedir(outdir_temp)
 
 #%% Now Start the loop
@@ -237,142 +258,5 @@ for xx in tqdm(range(nx-1)):
             
             
             
-        
-            
-            
-    
-        
-        
-        
-        
-        
-        
-        
 
-#%%
-
-
-
-
-
-#%%
-
-
-# #%% Open view of OISST (8GB for global)
-
-# st     = time.time()
-# ncpath = "/home/niu4/gliu8/share/OISST/mergetest/anom_detrend2_19820101-20251231/"
-# ncname = "oisst_day_1_sst_anom.nc"
-# dsview = xr.open_dataset(ncpath+ncname).load()
-# xrname = '__xarray_dataarray_variable__'
-# dsview = dsview.convert_calendar('noleap') # Remove Leap Year
-# print("Loaded Data in %.2fs" % (time.time()-st))
-# print(dsview)
-
-# 
-
-# winsize = 5
-
-#%%
-
-
-#doyall  = ds.time.dt.dayofyear
-nlags   = 365
-lags    = np.arange(nlags+1)
-days    = np.arange(1,366)
-ds      = dsview.squeeze()[xrname]
-
-years   = ds.time.dt.year
-ystart  = years[0].data.item()
-yend    = years[-1].data.item()
-
-if winsize == 0:
-    
-    #lagcorr_alldoys = []
-    for doybase in tqdm(np.arange(1,366)):
-        
-        base_var    = ds.sel(time=ds.time.dt.dayofyear.isin(doybase))
-        
-        lagcorr_day = []
-        for ll in range(nlags):
-            
-            # Select Lag Variable
-            doylag = doybase + ll
-            if doylag > 365:
-                doylag = doylag % 365
-                year_cross = True
-            else:
-                year_cross = False
-            lag_var  = ds.sel(time=ds.time.dt.dayofyear.isin(doylag))
-            
-            if year_cross:
-                base_slice  = ["%04i-01-01" % (ystart), "%04i-12-31" % (yend-1)] 
-                lag_slice   = ["%04i-01-01" % (ystart+1), "%04i-12-31" % (yend)] 
-                
-                base_var_in = base_var.sel(time=slice(*base_slice))
-                lag_var_in  = lag_var.sel(time=slice(*lag_slice))
-            
-            else:
-                base_var_in = base_var
-                lag_var_in  = lag_var
-            
-            lag_var_in['time'] = base_var_in['time']
-            
-            corrout = xr.corr(base_var_in,lag_var_in,dim='time')
-            lagcorr_day.append(corrout)
-            
-        lagcorr_day = xr.concat(lagcorr_day,dim='lag')
-        
-        outname = "%sdaily_acf_lagmax%i_nowindow_doy%03i.nc" % (outdir,nlags,doybase)
-        lagcorr_day.to_netcdf(outname)
-else:
-    print("Running script with window size")
-    
-    for doybase in tqdm(np.arange(1,366)):
-        
-        
-        # Construct Base Window
-        baseinfo    = proc.construct_window_doy(doybase,winsize,verbose=False)
-        
-        lagcorr_day = []
-        for ll in tqdm(range(nlags)):
-            
-            # Select Lag Variable
-            doylag = doybase + ll
-            if doylag > 365:
-                doylag = doylag % 365
-                year_cross = True
-            else:
-                year_cross = False
-            
-            # Construct Lag Widnow
-            laginfo = proc.construct_window_doy(doylag,winsize,verbose=False)
-            
-            # Perform Indexing
-            basevar,lagvar = proc.index_year_base_lag_xr(ds,baseinfo,laginfo,ll,verbose=False)
-            
-            # Calcualte xrcorr
-            lagvar['time'] = basevar['time']
-            corrout = xr.corr(basevar,lagvar,dim='time')
-            lagcorr_day.append(corrout)
-        
-        lagcorr_day = xr.concat(lagcorr_day,dim='lag')
-        outname = "%sdaily_acf_lagmax%i_winsize%02i_doy%03i.nc" % (outdir,nlags,winsize,doybase)
-        lagcorr_day.to_netcdf(outname)
-        
-        # End DOY Loop
-
-print("Completed Script in %.2fs" % (time.time()-st_all))
-    
-
-# #%% Quick Test if I just use xrcorr
-
-# doy_monstart = proc.get_doy_monstart()
-
-# st          = time.time()
-# ll      = 1
-
-# doybase = ds.sel(time=ds.time.dt.dayofyear.isin(1))
-
-# print("Caluclated Lag in %.2fs" % (time.time()-st))
 
